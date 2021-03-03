17ac9b7c0da844d246c22b5f91db7747f0349006
Stranuk <79793045+Stranuk@users.noreply.github.com>
2021-03-03 13:47:27 +0300

    Create l2-2.py

---
+++ ext2/l2-2.py
@@ -0,0 +1,6 @@
+from transliterate import translit
+
+
+print(translit("Lorem ipsum dolor sit amet", 'hy'))
+print(translit("Lorem ipsum dolor sit amet", 'ka'))
+print(translit("Lorem ipsum dolor sit amet", 'uk'))
