632c7da6060569f649f12bf113379e8d9a7f2f09
Stranuk <79793045+Stranuk@users.noreply.github.com>
2021-03-13 17:58:56 +0300

    Update l4-9.py

--- ext4/l4-9.py
+++ ext4/l4-9.py
@@ -1,7 +1,3 @@
 from PIL import Image
 
-image = Image.open("avatar.jpg")
-print(image.size) 
-image.thumbnail((80, 80))  
-print(image.size)
-image.save("avatar_thumbnail.jpg")
+image = Image.open("monro.jpg")
